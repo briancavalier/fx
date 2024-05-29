a8757c16ea0f44ba7a162aa21d14ff2ebc88486e
Brian Cavalier <brian@briancavalier.com>
2024-05-29 11:00:25 -0400

    Add TimeStep dispose and waitAll

--- src/internal/time.ts
+++ src/internal/time.ts
@@ -11,6 +11,7 @@ export class TimeStep {
   private _target = 0
   private _tasks: ScheduledTask[] = []
   private _timeout: any
+  private _disposed = false
 
   constructor(public readonly nowOrigin: number) { }
 
@@ -26,32 +27,24 @@ export class TimeStep {
     return this._target
   }
 
-  step(millis: number): void {
-    this._target = this._target + Math.max(0, millis)
-    return this._runTasks()
+  get disposed(): boolean {
+    return this._disposed
   }
 
-  private _runTasks(): void {
-    if (this._timeout) {
-      clearTimeout(this._timeout)
-      this._timeout = undefined
-    }
+  [Symbol.dispose](): void {
+    this._disposed = true
+    this._tasks = []
+    this.clearTimeout()
+  }
 
-    this._timeout = setTimeout(_ => {
-      if (this._tasks.length === 0) {
-        this._monotonic = this._target
-        return
-      }
+  step(millis: number): Promise<void> {
+    if (this._disposed) return Promise.resolve()
+    this._target = this._target + Math.max(0, millis)
+    return this.runTasks()
+  }
 
-      const start = this._monotonic
-      this._tasks.sort((a, b) => a.at - b.at)
-      if (this._tasks[0].at <= this._target) {
-        const t = this._tasks.shift()!
-        this._monotonic = t.at
-        t.task()
-        this._runTasks()
-      }
-    }, 0)
+  waitAll(): Promise<void> {
+    return this.step(Infinity)
   }
 
   handle = <E, A>(f: Fx<E, A>): Fx<Exclude<E, Now | Monotonic | Schedule>, A> => f.pipe(
@@ -61,7 +54,7 @@ export class TimeStep {
       const time = this._monotonic + Math.max(0, at)
       const t = { at: time, task }
       this._tasks.push(t)
-      if (time < this._target) this._runTasks()
+      if (time < this._target) this.runTasks()
 
       return ok({
         [Symbol.dispose]: () => {
@@ -71,4 +64,38 @@ export class TimeStep {
       })
     })
   ) as Fx<Exclude<E, Now | Monotonic | Schedule>, A>
+
+  private clearTimeout() {
+    if (this._timeout) {
+      clearTimeout(this._timeout)
+      this._timeout = undefined
+    }
+  }
+
+  private runTasks(): Promise<void> {
+    return this._disposed
+      ? Promise.resolve()
+      : new Promise(resolve => this.runTaskLoop(resolve))
+  }
+
+  private runTaskLoop(resolve: () => void): void {
+    this._timeout = setTimeout(_ => {
+      // More tasks may have been added between timeouts,
+      // so have to sort each time
+      this._tasks.sort((a, b) => a.at - b.at)
+
+      if (this._disposed || this._tasks.length === 0 || this._tasks[0].at > this._target) {
+        this._monotonic = this._target
+        return resolve()
+      }
+
+      const start = this._monotonic
+      if (this._tasks[0].at <= this._target) {
+        const t = this._tasks.shift()!
+        this._monotonic = t.at
+        t.task()
+        this.runTaskLoop(resolve)
+      }
+    }, 0)
+  }
 }

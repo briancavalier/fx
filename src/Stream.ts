dff17e814fe62935a44278d7827834075075d6e3
Brian Cavalier <brian@briancavalier.com>
2024-05-30 13:00:52 -0400

    Add Handle type (#15)
    * Add Handle type that replaces effects only when they may be produced in the first place
    
    * Remove handleIso
    
    * Simplify switchMap internal type

--- src/Stream.ts
+++ src/Stream.ts
@@ -13,7 +13,7 @@ export class Stream<A> extends Effect.Effect('fx/Stream')<A, void> { }
 
 export type Event<T> = T extends Stream<infer A> ? A : never
 
-export type ExcludeStream<E> = Exclude<E, Stream<any>>
+export type ExcludeStream<E, E2 = never> = Fx.Handle<E, Stream<any>, E2>
 
 /**
  * Emit a value
@@ -23,27 +23,27 @@ export const emit = <const A>(a: A) => new Stream(a)
 /**
  * Apply an effectul function to each value in a stream
  */
-export const forEach = <E, R, E2>(fx: Fx.Fx<E, R>, f: (a: Event<E>) => Fx.Fx<E2, void>): Fx.Fx<ExcludeStream<E> | E2, R> =>
+export const forEach = <E, R, E2>(fx: Fx.Fx<E, R>, f: (a: Event<E>) => Fx.Fx<E2, void>): Fx.Fx<ExcludeStream<E, E2>, R> =>
   fx.pipe(Fx.handle(Stream, a => f(a as Event<E>)))
 
 /**
  * Transform each value in a stream
  */
-export const map = <E, A, B>(fx: Fx.Fx<E, A>, f: (a: Event<E>) => B): Fx.Fx<ExcludeStream<E> | Stream<B>, A> =>
+export const map = <E, A, B>(fx: Fx.Fx<E, A>, f: (a: Event<E>) => B): Fx.Fx<ExcludeStream<E, Stream<B>>, A> =>
   forEach(fx, a => emit(f(a)))
 
 /**
  * Drop values from the stream that don't satisfy the predicate
  */
 export const filter: {
-  <E, A, B extends Event<E>>(fx: Fx.Fx<E, A>, refinement: (a: Event<E>) => a is B): Fx.Fx<ExcludeStream<E> | Stream<B>, A>
-  <E, A>(fx: Fx.Fx<E, A>, predicate: (a: Event<E>) => boolean): Fx.Fx<ExcludeStream<E> | Stream<Event<E>>, A>
-} = <E, A>(fx: Fx.Fx<E, A>, predicate: (a: Event<E>) => boolean): Fx.Fx<ExcludeStream<E> | Stream<Event<E>>, A> =>
+  <E, A, B extends Event<E>>(fx: Fx.Fx<E, A>, refinement: (a: Event<E>) => a is B): Fx.Fx<ExcludeStream<E, Stream<B>>, A>
+  <E, A>(fx: Fx.Fx<E, A>, predicate: (a: Event<E>) => boolean): Fx.Fx<E, A>
+} = <E, A>(fx: Fx.Fx<E, A>, predicate: (a: Event<E>) => boolean): Fx.Fx<ExcludeStream<E, Stream<Event<E>>>, A> =>
     forEach(fx, a => predicate(a) ? emit(a) : Fx.unit)
 
-export const switchMap = <E, X, E2>(fx: Fx.Fx<E, X>, f: (a: Event<E>) => Fx.Fx<E2, unknown>): Fx.Fx<Fork.Fork | Async.Async | ExcludeStream<E> | E2, X> =>
+export const switchMap = <E, X, E2>(fx: Fx.Fx<E, X>, f: (a: Event<E>) => Fx.Fx<E2, unknown>): Fx.Fx<ExcludeStream<E, Fork.Fork | Async.Async | E2>, X> =>
   Fx.bracket(
-    Fx.sync(() => new CurrentTask<ExcludeStream<E> | E2>()),
+    Fx.sync(() => new CurrentTask<E2>()),
     task => Fx.sync(() => dispose(task)),
     task => Fx.fx(function* () {
       const x = yield* forEach(fx, a => task.run(f(a)))
@@ -52,7 +52,7 @@ export const switchMap = <E, X, E2>(fx: Fx.Fx<E, X>, f: (a: Event<E>) => Fx.Fx<E
 
       return x
     })
-  )
+  ) as Fx.Fx<ExcludeStream<E, Fork.Fork | Async.Async | E2>, X>
 
 /**
  * Create a stream that emits all values from a {@link Queue.Dequeue}

c8ba1ec5315cb9946bdda25ace05ef5497d801e0
Brian Cavalier <brian@briancavalier.com>
2025-06-08 20:08:32 -0400

    Simplify retry example

--- examples/effect-ts/retry.ts
+++ examples/effect-ts/retry.ts
@@ -1,38 +1,35 @@
 import { Async, Effect, Fail, Fork, Fx, Log, handle, runTask, tap } from "../../src"
 
-// Fx doesn't have a built-in HTTP client, but we can create
-// a simple GetJson effect and a handler that uses the Fetch API.
+// fx doesn't have a built-in HTTP client, but we can create
+// a simple GetJson effect
 class GetJson extends Effect("Http")<string, unknown> { }
 
-const getJson = (url: string) => new GetJson(url)
-
+// and a handler that uses the Fetch API.
 const withFetchGetJson = handle(GetJson, (url) =>
   Async.tryPromise(signal =>
     fetch(new URL(url, 'https://jsonplaceholder.typicode.com/'), { signal })
       .then(res => res.ok
         ? res.json()
         : Promise.reject(new Error(`Failed to fetch ${url}: ${res.statusText}`)))
-  )
-)
+  ))
 
 // We can also create a simple retry handler easily
 const retry = (tries: number) => <E, A>(fa: Fx<E, A>): Fx<E, A> =>
   fa.pipe(Fail.catchAll((e) =>
-    (tries === 0 ? Fail.fail(e) : fa.pipe(retry(tries - 1))) as Fx<E, A>
+    (tries > 1 ? fa.pipe(retry(tries - 1)) : Fail.fail(e)) as Fx<E, A>
   ))
 
-// Define findUserById function that using the new GetJson effect
-const findUserById = (id: string) => getJson(`/users/${id}`)
+// Define findUserById using the new GetJson effect
+const findUserById = (id: string) => new GetJson(`/users/${id}`)
 
-const main = findUserById("1").pipe(
-  tap(user => Log.info(`Got user`, { user })),
-).pipe(
+const main = findUserById('123').pipe(
   withFetchGetJson,
   retry(3)
 )
 
 main.pipe(
-  Fail.catchAll(e => Log.error(`Error fetching user: ${e}`)),
+  tap(user => Log.info('Got user', { user })),
+  Fail.catchAll(error => Log.error(`Error fetching user`, { error })),
   Log.console,
   Fork.unbounded,
   runTask

5b4836bc2ebc40a539ca5750229aa768704c9ad6
Brian Cavalier <brian@briancavalier.com>
2026-05-06 19:51:30 -0400

    add async origin example

---
+++ examples/concurrency/async-origin-stack-traces.ts
@@ -0,0 +1,24 @@
+import { fx, runPromise } from "../../src"
+import { assertPromise } from "../../src/Async"
+import { fork, unbounded } from "../../src/Concurrent"
+import { defaultConsole, error, log } from "../../src/Console"
+import { catchAll } from "../../src/Fail"
+import { wait } from "../../src/Task"
+
+const loadUser = assertPromise(async () => {
+  await Promise.resolve()
+  throw new Error('loadUser request failed')
+})
+
+const program = fx(function* () {
+  yield* log('forking loadUser')
+  const task = yield* fork(loadUser)
+  return yield* wait(task)
+})
+
+await program.pipe(
+  catchAll(e => error('Error!', e)),
+  unbounded,
+  defaultConsole,
+  runPromise
+)

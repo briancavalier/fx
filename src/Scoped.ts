7656882843d5c6b7a49d7d06856b0e6bb9c9ddb4
Brian Cavalier <brian@briancavalier.com>
2026-06-01 19:40:30 -0400

    Simplify scoped export

--- src/Scoped.ts
+++ src/Scoped.ts
@@ -12,12 +12,9 @@ type PrivateScope = AnyLifetimeScope & {
  *
  * Use `currentScope` inside the Fx to target the private boundary.
  */
-export function scoped<const E, const A>(
+export const scoped = <const E, const A>(
   f: Fx<E, A>
-): Fx<ScopeEffects<E, PrivateScope>, A>
-export function scoped<const E, const A>(
-  f: Fx<E, A>
-): Fx<ScopeEffects<E, PrivateScope>, A> {
+): Fx<ScopeEffects<E, PrivateScope>, A> => {
   const privateScope = scope<PrivateScope>()(Symbol('fx/Scoped/scoped'), { diagnostic: false })
   return f.pipe(withScope(privateScope)) as Fx<ScopeEffects<E, PrivateScope>, A>
 }
